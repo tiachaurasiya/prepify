a2bd08aff96aa4190c60f90e3db6b7ba3a2cd8de
Tia Chaurasiya <tiachh73@gmail.com>
2024-06-24 14:43:26 -0400

    Initial Commit

---
+++ prepify/prepifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  prepifyApp.swift
+//  prepify
+//
+//  Created by tia on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct prepifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
