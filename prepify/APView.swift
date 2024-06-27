3e86ac8ecb3d3c955756d56c72a49b57b29b5520
Emma Lopez <25lopezev@rbhs208.net>
2024-06-27 10:26:19 -0500

    ap page

--- prepify/APView.swift
+++ prepify/APView.swift
@@ -1,5 +1,5 @@
 //
-//  APView.swift
+//  IBView.swift
 //  prepify
 //
 //  Created by Scholar on 6/26/24.
@@ -9,10 +9,199 @@ import SwiftUI
 
 struct APView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        NavigationStack {
+            
+            VStack {
+                Text("AP Resources")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("#003f94ff"))
+                Text("Here are free resources")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 55)
+                Text("for AP. Good luck scholar!")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 55)
+        
 
+            
+                NavigationLink(destination: ResourcesView()) {
+                    Text("AP Capstone Diploma")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 40)
+                        .frame(height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+                
+                NavigationLink(destination: ResourcesView()) {
+                    Text("AP English")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 95)
+                        .frame(height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+                
+                NavigationLink(destination: ResourcesView()) {
+                    Text("AP History + Social Science")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 10)
+                        .frame(height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+                
+                NavigationLink(destination: ResourcesView()) {
+                    Text("AP Math + Computer Science")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 1)
+                        .frame(height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+                
+                NavigationLink(destination: ResourcesView()) {
+                    Text("AP Sciences")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 88.5)
+                        .frame(height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+                
+                NavigationLink(destination: ResourcesView()) {
+                    Text("AP World Language + Culture")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 1)
+                        .frame(height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+                
+                NavigationLink(destination: ResourcesView()) {
+                    Text("Additional Resources")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 42.5)
+                        .frame(height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+                
+            }
+            
+            .padding(.top, 30)
+            
+            // Bottom Navigation
+            
+            NavigationStack {
+                VStack {
+                    Spacer()
+
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color("#003f94ff"))
+                        .frame(height: 90)
+                        .padding(.horizontal)
+                        .shadow(radius: 5)
+                        .overlay(
+                            HStack(spacing: 20) {
+                                // First back icon
+                                NavigationLink(destination: ContentView()) {
+                                    Image("icons8-home-50")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 40, height: 40)
+                                      
+                                }
+                                Spacer()
+                                
+                                // Second book icon
+                                NavigationLink(destination: ResourcesView()) {
+                                    Image( "icons8-book-50")
+                                                                   .resizable()
+                                                                   .aspectRatio(contentMode: .fit)
+                                                                   .frame(width: 40, height: 40)
+                                                        
+                                                                   
+                                }
+                                
+                             Spacer()
+                                // Third Clock icon
+                                NavigationLink(destination: PomodoroView()) {
+                                    Image( "icons8-clock-50")
+                                                                   .resizable()
+                                                                   .aspectRatio(contentMode: .fit)
+                                                                   .frame(width: 40, height: 40)
+                                                        
+                                                            
+                                                                   
+                                }
+                                
+                                Spacer()
+                                // Fourth Checkmark icon
+                                NavigationLink(destination: ToDoView()) {
+                                    Image( "icons8-check-mark-50")
+                                                                   .resizable()
+                                                                   .aspectRatio(contentMode: .fit)
+                                                                   .frame(width: 40, height: 40)
+                                                        
+                                                            
+                                                                   
+                                }
+                                // End icons
+                                                        }
+                                                        .padding(.horizontal, 70)
+                                                    )
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+
+    struct bottomnavigation: View {
+        var body: some View {
+            Text("Home View")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+            
+            
+            
+        }
+        
 #Preview {
     APView()
 }
