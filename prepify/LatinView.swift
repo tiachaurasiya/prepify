17cd9dcac59a033bb00730081c1ea7c9dc62db2e
Jada <jadahernandez0828@gmail.com>
2024-06-28 00:31:25 -0400

    world language resources buttons no links

--- prepify/LatinView.swift
+++ prepify/LatinView.swift
@@ -9,7 +9,66 @@ import SwiftUI
 
 struct LatinView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack {
+            
+            VStack {
+                
+                // TITLE
+                
+                Text("AP Latin")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("#003f94ff"))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 45)
+                
+                //Knowt
+                
+                Link(destination: URL(string: "https://docs.google.com/document/d/1EDJ-fAzLmxKreeMn4hFfrdzSeLSfw4Sye1eSD0N3jWw/edit?pli=1#heading=h.2iozmvk7y6d7")!) {
+                    Text("Knowt")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 40)
+                        .frame(width: 300.0, height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+                
+                //Fiveable
+                
+                Link(destination: URL(string: "https://apresources.notion.site/AP-Seminar-378d59f8ee2e45fe8624ad45d92432b1")!) {
+                    Text("Fiveable")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 37.5)
+                        .frame(width: 300.0, height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+                
+                //AP Daily
+                
+                Link(destination: URL(string: "https://knowt.com/exams/AP/AP-Seminar")!) {
+                    Text("AP Daily")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 97.5)
+                        .frame(width: 300.0, height: 45)
+                        .foregroundColor(Color.white)
+                        .background(Color("#003f94ff"))
+                }
+                
+                .buttonStyle(.borderedProminent)
+                .tint(Color("#003f94ff"))
+            }
+        }
     }
 }
 
